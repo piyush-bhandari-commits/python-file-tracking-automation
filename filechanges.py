70f96b379a02707cb7bb02cf7626ff0df7c57cee
Piyush Bhandari <piyush.bhandari@outlook.com>
2020-12-23 16:53:59 -0500

    connected to db

---
+++ filechanges.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import sqlite3
+from sqlite3 import Error
+
+"""Returns the name of the SQLite DB file"""
+def getbasefile():
+    return os.path.splitext(os.path.basename(__file__))[0]
+
+"""Connects to the SQLite DB"""
+def connectdb():
+    try:
+        dbfile = getbasefile() + '.db'
+        conn = sqlite3.connect(dbfile , timeout=2)
+
+    except Error as e:
+        print(e)
+
+    finally:
+        if conn:
+            conn.close()
+            print("The conncection is closed to the database")
