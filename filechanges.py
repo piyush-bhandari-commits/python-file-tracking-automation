e2bd9135a3441498a487f2681a1c91e857c2e703
Piyush Bhandari <piyush.bhandari@outlook.com>
2020-12-24 17:54:11 -0500

    check if table exists

--- filechanges.py
+++ filechanges.py
@@ -3,12 +3,17 @@ import sys
 import sqlite3
 from sqlite3 import Error
 
-"""Returns the name of the SQLite DB file"""
+
 def getbasefile():
+    """
+    Returns the name of the SQLite DB file
+    """
     return os.path.splitext(os.path.basename(__file__))[0]
 
-"""Connects to the SQLite DB"""
 def connectdb():
+    """
+    Connects to the SQLite DB
+    """
     try:
         dbfile = getbasefile() + '.db'
         conn = sqlite3.connect(dbfile , timeout=2)
@@ -23,4 +28,49 @@ def connectdb():
             print("The connection is closed to the database")
 
 
-connectdb()
+def corecursor(conn, query, args):
+    """
+    Opens the sqlite database cursor
+    """
+    result = False
+    cur = conn.cursor()
+    
+    try:
+        cur.execute(query, args)
+        rows = cur.fetchall()
+        numrows = len(list(rows))
+        if numrows > 0:
+            result = True
+    except Error as e:
+        print(e)
+    finally:
+        if cur != None:
+            cur.close()
+    
+    return result
+
+
+
+def tableexists(table):
+    """
+    Checks if a SQLite DB Table exists
+    """
+    result = False
+    try:
+        conn = connectdb()
+        if conn != None:
+                   query = "SELECT name FROM sqlite_master WHERE type ='table' AND name NOT LIKE 'sqlite_%'" # Finish this query ...
+                   args = (table,)
+                   result = corecursor(conn, query, args)
+    except Error as e:
+        print(e)
+    
+    finally:
+        if conn != None:
+            conn.close()
+
+    
+    
+
+if __name__ == "__main__":
+    connectdb()
